edb96a777ab69a48f3543ea729d3a7a1576f0ccd
Munzy <cmunroe@cameronmunroe.com>
2018-01-24 08:59:35 -0800

    Outage Page Fixes
    Making things work nicely!

--- includes/example-config.php
+++ includes/example-config.php
@@ -15,23 +15,51 @@ $servers[] = array(
 	'name' => 'Example Host', 
 	'location' => 'Dallas Texas', 
 	'host' => 'Example Host', 
-	'type' => 'Test File'
+	'type' => 'Test File',
+	'maxload' => 2.0
 );
 
 
 
 
 
-$template = "./templates/default/"; // currently default and dark are the only options
-$index = $template . "index.php"; 
-$refresh = 10005; // this defines how quickly clients will recheck the uptime server for updates
-$cache = 10; // this is how long before the cache expires, set to 0 to turn off.
-$failafter = 60; // this defines what we see as an outage or not in seconds. 
-// (cont.) This protects against failed queries unless there is an actual outage for more then X. 
-$rtype = 'free'; // this is at the bottom of the page  and will tell your users if you are showing 
-// free or used resources to make it more clear for them!
-$mailme = 1; // anything other then 1 will make it not send emails to you!
-$emailto = '@gmail.com'; // where should we send down and up alerts?
-$emailfrom = 'serverstatus@'; // where will we send the emails from?
-$no_stripe = 1; // Turns off the stripe animation to improve performance
+
+//============================================================================================
+// Chose A template.
+// Options ( dark | default | default2016 )
+$template = "./templates/default/"; 
+ 
+// Tells the web client how often to recheck in ms.
+$refresh = 10005; 
+
+ // This is how long before the cache expires in seconds.
+$cache = 10;
+
+// This defines how many seconds before we define a server down.
+$failafter = 70;  
+
+
+
+// If you query uptime.php than use free, if you query uptime_used than use used.
+$rtype = 'free'; 
+
+
+// Anything other then 1 will make it not send emails to you!
+$mailme = 0; 
+
+
+// Email Options via phpmail.
+// where should we send down and up alerts?
+$emailto = '@gmail.com'; 
+// where will we send the emails from?
+$emailfrom = 'serverstatus@'; 
+
+//============================================================================================
+// Settings below this shouldn't be changed!
+
+$index = $template . "index.php";
+
+// Turns off the stripe animation to improve performance.
+$no_stripe = 1; 
+
 ?>
